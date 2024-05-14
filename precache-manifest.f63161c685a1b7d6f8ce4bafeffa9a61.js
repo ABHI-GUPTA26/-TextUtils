e44e19998d816cdf9cd6a3697a82420940fcd161
ABHI-GUPTA26 <158084578+ABHI-GUPTA26@users.noreply.github.com>
2024-05-14 18:16:07 +0530

    Updates

---
+++ precache-manifest.f63161c685a1b7d6f8ce4bafeffa9a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5706c698812496d0f521022b2ef80f71",
+    "url": "/-TextUtils/index.html"
+  },
+  {
+    "revision": "3f72fecb054af5a999db",
+    "url": "/-TextUtils/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8946a0edaa5c8cb07476",
+    "url": "/-TextUtils/static/js/2.2a068b1b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/-TextUtils/static/js/2.2a068b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8acca07efd9b80fc433",
+    "url": "/-TextUtils/static/js/3.f665a10a.chunk.js"
+  },
+  {
+    "revision": "3f72fecb054af5a999db",
+    "url": "/-TextUtils/static/js/main.d9f44e58.chunk.js"
+  },
+  {
+    "revision": "0408950ad9a0a7e85f73",
+    "url": "/-TextUtils/static/js/runtime-main.961256f8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/-TextUtils/tree/master/precache-manifest.c98711fd659704065e9e25fd8befcc62.js"
+  "/-TextUtils/precache-manifest.f63161c685a1b7d6f8ce4bafeffa9a61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/-TextUtils/tree/master/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/-TextUtils/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
